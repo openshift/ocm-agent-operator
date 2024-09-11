c3ac8edc9f136dca214053e24f0e696ea1722833
Florian Bergmann <fbergman@redhat.com>
2024-09-11 15:16:20 +0200

    Update the kubebuilder check.

--- api/v1alpha1/managednotification_types.go
+++ api/v1alpha1/managednotification_types.go
@@ -57,7 +57,7 @@ type Notification struct {
 	// References useful for context or remediation - this could be links to documentation, KB articles, etc
 	References []NotificationReferenceType `json:"references,omitempty"`
 
-	// +kubebuilder:validation:Enum={"Debug","Info","Warning","Error","Fatal"}
+	// +kubebuilder:validation:Enum={"Debug","Info","Warning","Major","Critical"}
 	// The severity of the Service Log notification
 	Severity NotificationSeverity `json:"severity"`
 
